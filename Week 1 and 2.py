ae00e086f24030c38a9d8d4cb2f0549d04deafaa
Shemaiah001 <petershemaiah003@gmail.com>
2025-08-19 12:30:31 +0100

    Add initial Python script and VSCode settings
    - Created a new Python script 'Week 1 and 2.py' with examples of string manipulation, list operations, and comments explaining each method.
    - Added a VSCode settings file to set the color theme to 'Cursor Dark'.

---
+++ Week 1 and 2.py
@@ -0,0 +1,48 @@
+# # # message= "Hello Eric"
+
+# # # greeting= "Welcome to the world of Python"
+
+# # # full_greeting= message + "\n" + greeting
+
+# # # print(full_greeting)
+
+# # favourite_number= 13
+
+# # new_message= f"My favourite number is {favourite_number}"
+
+# # #This is a f-string. It is a string that contains a variable. And it is used to insert the variable into the string.
+# # print(new_message)
+
+
+# # import this #This is a module that contains the Zen of Python. It is a collection of quotes about the philosophy of Python.
+
+friends= ["Eric", "John", "Michael", "Terry", "Graham"]
+# Square brackets are used to access elements of a list.
+# The first element of a list is at index 0.
+# The last element of a list is at index -1.
+# The second last element of a list is at index -2.
+# The third last element of a list is at index -3.
+# And so on.
+
+# friends.append("Maiah")
+# # The append method adds an element to the end of the list.
+
+friends.insert(3, "Maiah")
+#The insert method adds an element to the list in a specific position
+
+friends.remove("Eric")
+#The remove method removes an element from the list
+
+friends.pop()
+#The pop method removes the last element from the list
+
+friends.sort()
+#The sort method sorts the list in ascending order
+
+friends.list
+
+
+
+
+
+
