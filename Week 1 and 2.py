f0676a7d02e7576e19d31e08401f0f007a6b207a
Shemaiah001 <petershemaiah003@gmail.com>
2025-08-19 12:43:25 +0100

    commit

--- Week 1 and 2.py
+++ Week 1 and 2.py
@@ -16,33 +16,35 @@
 
 # # import this #This is a module that contains the Zen of Python. It is a collection of quotes about the philosophy of Python.
 
-friends= ["Eric", "John", "Michael", "Terry", "Graham"]
-# Square brackets are used to access elements of a list.
-# The first element of a list is at index 0.
-# The last element of a list is at index -1.
-# The second last element of a list is at index -2.
-# The third last element of a list is at index -3.
-# And so on.
+# friends= ["Eric", "John", "Michael", "Terry", "Graham"]
+# # Square brackets are used to access elements of a list.
+# # The first element of a list is at index 0.
+# # The last element of a list is at index -1.
+# # The second last element of a list is at index -2.
+# # The third last element of a list is at index -3.
+# # And so on.
 
-# friends.append("Maiah")
-# # The append method adds an element to the end of the list.
+# # friends.append("Maiah")
+# # # The append method adds an element to the end of the list.
 
-friends.insert(3, "Maiah")
-#The insert method adds an element to the list in a specific position
+# friends.insert(3, "Maiah")
+# #The insert method adds an element to the list in a specific position
 
-friends.remove("Eric")
-#The remove method removes an element from the list
+# friends.remove("Eric")
+# #The remove method removes an element from the list
 
-friends.pop()
-#The pop method removes the last element from the list
+# friends.pop()
+# #The pop method removes the last element from the list
 
-friends.sort()
-#The sort method sorts the list in ascending order
-
-friends.list
+# friends.sort()
+# #The sort method sorts the list in ascending order
 
 
+invited_friends=["Maiah", "Ruth", "Him"]
 
+for name in invited_friends:
+    message= f"Hi {name}, you are invited to a party"
+    print(message)
 
 
 
